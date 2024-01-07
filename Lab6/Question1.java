0a9616c4ed438ec1894db802ed4d42ce4f39243d
MANU MARTIN <111359942+ManuMartinDeveloper@users.noreply.github.com>
2024-01-07 10:58:19 +0530

    Lab 6

---
+++ Lab6/Question1.java
@@ -0,0 +1,12 @@
+package Lab6;
+//You are provided with an integer array `coins[]` of size N, representing different denominations
+// of currency, and an integer `sum`. The task is to find the number of ways you can make the given
+// sum by using different combinations from the coins[] array. Assume that you have an infinite
+// supply of each type of coin, and you can use any coin as many times as needed.Incorporate
+// multithreading concepts to optimize the computation of different combinations. Design a
+// concurrent solution that utilizes multiple threads to explore and calculate the various
+// combinations efficiently.
+
+public class Question1 {
+    
+}
