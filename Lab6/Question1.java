b599684cf1c468d3336767801e5e1d43023dd31a
MANU MARTIN <111359942+ManuMartinDeveloper@users.noreply.github.com>
2024-01-13 11:27:30 +0530

    Update Question1.java

--- Lab6/Question1.java
+++ Lab6/Question1.java
@@ -8,5 +8,52 @@ package Lab6;
 // combinations efficiently.
 
 public class Question1 {
-    
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the size of Array : ");
+        int n = input.nextInt();
+        int[] coins = new int[n];
+
+        System.out.println("Please enter the sum value : ");
+        int sum = input.nextInt();
+        Thread t1 = new Thread(() -> {
+            synchronized (coins) {
+                System.out.print("Please enter the coins value: ");
+                for (int i = 0; i < n; i++) {
+                    coins[i] = input.nextInt();
+                }
+            }
+        });
+        List<List<Integer>> v = new ArrayList<>();
+        Thread t2 = new Thread(() -> {
+            synchronized (coins) {
+                combiFunction(coins, sum, 0, new ArrayList<>(), v);
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        for (List<Integer> combination : v) {
+            System.out.println(combination);
+        }
+    }
+
+    static void combiFunction(int[] array, int target, int start, List<Integer> current,
+            List<List<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i < array.length; i++) {
+            if (array[i] <= target) {
+                current.add(array[i]);
+                combiFunction(array, target - array[i], i, current, result);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
 }
