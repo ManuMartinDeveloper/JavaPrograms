b6e121b2122281f715b6ec2870bb5a2ba7223773
MANU MARTIN <111359942+ManuMartinDeveloper@users.noreply.github.com>
2024-01-13 12:59:31 +0530

    creation of program

---
+++ Lab8/AttendeeInfo.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class christConference{
+    public add_attendees(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the attendees details: ");
+        String name = scan.nextLine();
+    }
+}
+
